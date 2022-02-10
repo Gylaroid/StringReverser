d55cf9aec4baa56ea732370dd3a01fd005b7a646
Gylaroid <sanshez56@gmail.com>
2022-02-10 14:53:40 +0500

    Deleted defaultReverser, splitReverser.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
 public class Main {
     public static void main(String[] args){
         String str = "The some string";
-        defaultReverser(str);
-        splitReverser(str);
         strBuilderReverser(str);
         alternativeReverser(str);
-    }
-
-    //Warning: String concatenation '+=' in loop
-    private static void defaultReverser(String str){
-        String newStr = "";
-        for(int i = str.length() - 1; i >= 0; i--){
-            newStr += str.charAt(i);
-        }
-        System.out.println(newStr);
-    }
-
-    //Warning: String concatenation '+=' in loop
-    private static void splitReverser(String str){
-        String newStr = "";
-        String[] stringArray = str.split("");
-        for(int i = stringArray.length - 1; i >= 0; i--){
-            newStr += stringArray[i];
-        }
-        System.out.println(newStr);
     }
 
     private static void strBuilderReverser(String str){
