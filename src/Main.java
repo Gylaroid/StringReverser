364b0486c625b6a580bedda93052025c6e59d391
Gylaroid <sanshez56@gmail.com>
2022-02-10 14:24:32 +0500

    Implemented alternative reverser

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 public class Main {
     public static void main(String[] args){
         String str = "The some string";
-//        String newStr = "";
-//        for(int i = str.length() - 1; i >= 0; i--){
-//            newStr += str.charAt(i);
-//        }
-//        System.out.println(newStr);
-//        newStr = "";
+        defaultReverser(str);
+        splitReverser(str);
+        strBuilderReverser(str);
+        alternativeReverser(str);
+    }
 
+    //Warning: String concatenation '+=' in loop
+    private static void defaultReverser(String str){
+        String newStr = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            newStr += str.charAt(i);
+        }
+        System.out.println(newStr);
+    }
 
-//        String[] stringArray = str.split("");
-//        for(int i = stringArray.length - 1; i >= 0; i--){
-//            newStr += stringArray[i];
-//        }
-//        System.out.println(newStr);
-//        newStr = "";
+    //Warning: String concatenation '+=' in loop
+    private static void splitReverser(String str){
+        String newStr = "";
+        String[] stringArray = str.split("");
+        for(int i = stringArray.length - 1; i >= 0; i--){
+            newStr += stringArray[i];
+        }
+        System.out.println(newStr);
+    }
 
-
+    private static void strBuilderReverser(String str){
         StringBuilder newBuilderStr = new StringBuilder(str.length());
         for(int i = str.length() - 1; i >= 0; i-- ){
             newBuilderStr.append(str.charAt(i));
         }
         System.out.println(newBuilderStr);
+    }
+
+    private static void alternativeReverser(String str){
+        char[] chArray = str.toCharArray();
+        for(int i = str.length() - 1; i >= 0; i--){
+            System.out.print(chArray[i]);
+        }
     }
 }
